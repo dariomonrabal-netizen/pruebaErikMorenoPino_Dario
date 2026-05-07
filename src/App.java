ed8a4c80b52b9a2f3703d57aaa500b80ac4e82f9
dario <dariomonrabal@gmail.com>
2026-05-07 19:32:31 +0200

    cambios 2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Erik Moreno Pino");
+         System.out.println("Ejercicio 8b");
     }
 }//cambios Dario Monrabal Vidal
